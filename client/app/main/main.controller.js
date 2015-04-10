26f4242afb17d0e1664e028e51760765cdb6f6b9
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-04-10 15:10:25 +0700

    Show data as table, next and pre button

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,19 +4,35 @@ angular.module('mongoWebQueryApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    //$http.get('/api/query').success(function(awesomeThings) {
-    //  $scope.awesomeThings = awesomeThings;
-    //});
+    $scope.page = 0;
+    $scope.perpage = 20;
 
     $scope.search = function() {
       if($scope.keyword === '') {
         return;
       }
 
-      $http.post('/api/query', { keyword: $scope.keyword }).success(function(data) {
+      $scope.loading = true;
+      $http.post('/api/query', { keyword: $scope.keyword, page: $scope.page }).success(function(data) {
           $scope.data = data;
+          $scope.loading = false;
       });
     };
+
+    $scope.nextPage = function() {
+      $scope.page = $scope.page + $scope.perpage;
+      $scope.search();
+    }
+
+    $scope.prePage = function() {
+      if ($scope.page - $scope.perpage < 0) {
+        alert('You cannot!');
+        return;
+      }
+
+      $scope.page = $scope.page - $scope.perpage;
+      $scope.search();
+    }
 
     $scope.export = function(item) {
       
