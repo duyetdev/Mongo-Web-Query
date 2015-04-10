5b6627a0c5cddce6090b124e61a1c80279736cfb
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-04-10 14:52:40 +0700

    Remove package mongoose-search-plugin

--- server/api/query/query.model.js
+++ server/api/query/query.model.js
@@ -1,17 +1,12 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-	searchPlugin = require('mongoose-search-plugin'),
     Schema = mongoose.Schema;
 
 var ContentsScheme = new mongoose.Schema({
 	url_id: String,
 	url: String, 
 	content: { type: String, index: { unique: true, dropDups: true }}
-});
-
-ContentsScheme.plugin(searchPlugin, {
-	fields: ['url', 'content']
 });
 
 module.exports = mongoose.model('Contents', ContentsScheme);
