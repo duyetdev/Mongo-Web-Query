01587150825aeca5ead1b978ee3ff4d155b22ce5
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-04-10 18:06:20 +0700

    Export by keyword

--- server/api/query/query.controller.js
+++ server/api/query/query.controller.js
@@ -37,13 +37,32 @@ exports.index = function(req, res) {
 
 
 exports.export = function(req, res) {
-  var keyword = req.body.keyword || '';
-  var q = Content.find({ "content": { "$regex": keyword, "$options": "i" } });
-  q.exec(function(err,docs) { 
-      if (err) console.log(err);
+  var keyword = req.param('keyword') || '';
 
-      return res.json(200, docs);
+  console.log('Getting with keyword %s', keyword);
+
+  var q = Content.find({ "content": { "$regex": keyword, "$options": "i" } }).select('url_id');
+  
+  q.exec(function(err, idList) { 
+      console.log(idList);
+
+      var list = [];
+
+      idList.forEach(function(i) {
+        list.push(i.url_id);
+      });
+
+      console.log(list);
+
+      Content.find().where('url_id').in(list).exec(function(err, data) {
+          res.send(200, JSON.stringify(data, null, 4));
+      });
   });
+
+  /*Content.find({ "content": { $regex: keyword, "$options": "i" } }).exec(function(err, data) {
+        if (err) console.log(err);
+        res.json(200, data);
+  });*/
 }
 
 function handleError(res, err) {
